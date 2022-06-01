debb9747e71be9961c31c3e7ce36c657f2561014
Soumwadeep Guha <soumwadeep@gmail.com>
2022-06-01 11:55:36 +0530

    Updated

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -42,15 +42,11 @@ const Content = () => {
             <hr className="d-sm-none" />
           </div>
           <div className="col-sm-8">
-            <h2>TITLE HEADING</h2>
-            <h5>Title description, Dec 7, 2020</h5>
+            <h2>LATEST UPLOADS</h2>
             <div className="fakeimg">Fake Image</div>
             <p>Some text..</p>
             <p>
-              Sunt in culpa qui officia deserunt mollit anim id est laborum
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco.
+              Added about.js,content.js,home.js,service.js in src folder
             </p>
 
             <h2 className="mt-5">TITLE HEADING</h2>
